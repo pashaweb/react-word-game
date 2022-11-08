7603d22994438763fb703cf1aa0de6712ddf9c45
pavelk <pavel.k@selectmedia.com>
2022-11-08 03:50:55 +0100

    Testing and testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import { IGameData } from './store/store';
+import '@testing-library/jest-dom/extend-expect';
 
 const gameData1: IGameData = {
   source_language: "cn",
@@ -37,9 +38,21 @@ describe('App and fails to load data', () => {
     jest.spyOn(global, 'fetch').mockImplementation(() => Promise.resolve({ 
       text: () => Promise.resolve(JSON.stringify(gameData1) + '\n' ) 
     }));
-    const {container} = render(<App />);
-    await screen.findByText(/SUCCESSLOADINGGAMES/i);
-    //console.log(container.innerHTML);
+      const {container} = render(<App />);
+      const gameStatsRendered = await screen.findByText(/Total Games: 1/i);
+      expect(gameStatsRendered).toBeInTheDocument();
+      const gameWon = await screen.findByText(/Games Won: 0/i);
+      expect(gameWon).toBeInTheDocument();
+      const currentGameNumber = await screen.findByText(/Current Game: 0/i);
+      expect(currentGameNumber).toBeInTheDocument();
+      const word = await screen.findByText(/Word: natus/i);
+      expect(word).toBeInTheDocument();
+      const translations = await screen.findByText(/Translations: {"0,0,1,1,2,2":"aei"}/i);
+      expect(translations).toBeInTheDocument();
+      const grid = container.querySelector('.game-grid-container');
+      expect(grid).toBeInTheDocument();
+      expect(grid?.children.length).toBe(9);
+      console.log(container.innerHTML);
   });
 
 
@@ -47,5 +60,8 @@ describe('App and fails to load data', () => {
 
 
 
+function act(arg0: () => Promise<void>) {
+  throw new Error('Function not implemented.');
+}
   // expect(linkElement).toBeInTheDocument();
 //});
